e0b4fe8d48d8ce906512a20d16d9868b7d62c584
Gerald2309 <gerald.e2309@gmail.com>
2024-10-10 22:37:50 -0500

    creacion proyecto base

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double salary = 1000;
+
+
+        System.out.println(MoneyUtil.format(salary));
+    }
+}
